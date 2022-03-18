62ad338f4e679b23c7d1b1f50ff153dc216d92a4
Gengchen <tuogengchen@gmail.com>
2022-03-18 13:29:02 +0000

    update

---
+++ build/CMakeFiles/MiraiCP.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/thallium/git/Mirai-competitive-programming/main.cpp" "/home/thallium/git/Mirai-competitive-programming/build/CMakeFiles/MiraiCP.dir/main.cpp.o"
+  "/home/thallium/git/Mirai-competitive-programming/single_include/MiraiCP/MiraiCP.cpp" "/home/thallium/git/Mirai-competitive-programming/build/CMakeFiles/MiraiCP.dir/single_include/MiraiCP/MiraiCP.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MiraiCP_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../single_include/3rd_include"
+  "../single_include/3rd_include/utf8"
+  "../single_include/3rd_include/utf8/utf8"
+  "../single_include/MiraiCP"
+  "/usr/lib/jvm/java-11-openjdk-amd64/include"
+  "/usr/lib/jvm/java-11-openjdk-amd64/include/linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- main.cpp
+++ main.cpp
@@ -4,19 +4,31 @@ using namespace MiraiCP;
 
 // 插件实例
 class Main : public CPPPlugin {
-public:
-  // 配置插件信息
-  Main() : CPPPlugin(PluginConfig("Plugin id", "Plugin name", "Version", "Author name", "Plugin description", "Publish time")) {}
-  void onEnable() override {
-    // 监听
-  }
+  public:
+    // 配置插件信息
+    Main()
+        : CPPPlugin(PluginConfig("mirai-cp", "Mirai Competitive Programming",
+                                 "0.0.1", "Thallium54",
+                                 "A QQ bot about competitve programming",
+                                 "Publish time")) {}
+    void onEnable() override {
+        // 监听
+        // Event::processor.registerEvent<GroupMessageEvent>(
+        //     [](GroupMessageEvent e) { 
+        //         e.group.sendMessage(e.message);
+        //     });
 
-  void onDisable() override {
-    /*插件结束*/
-  }
+        Event::processor.registerEvent<PrivateMessageEvent>(
+            [](PrivateMessageEvent e) {
+                if (e.message[0].type() == 0) {
+                    e.sender.sendMessage(e.message[0].get<PlainText>().content);
+                }
+            });
+    }
+
+    void onDisable() override { /*插件结束*/
+    }
 };
 
 // 绑定当前插件实例
-void MiraiCP::enrollPlugin() {
-  MiraiCP::enrollPlugin0(new Main());
-}
+void MiraiCP::enrollPlugin() { MiraiCP::enrollPlugin0(new Main()); }
